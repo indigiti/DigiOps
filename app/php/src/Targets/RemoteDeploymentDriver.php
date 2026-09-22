b55ecac04f5cfe0eef20ec14e77b06d39edb592c
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-22 18:32:47 +0530

    Distinguish uncertain remote commit responses

--- app/php/src/Targets/RemoteDeploymentDriver.php
+++ app/php/src/Targets/RemoteDeploymentDriver.php
@@ -52,12 +52,20 @@ final class RemoteDeploymentDriver
             fclose($fp);
         }
 
-        return $this->targets->remoteRequest($projectId,'deploy-commit',[
-            'project'=>$projectId,
-            'uploadId'=>$uploadId,
-            'publicPath'=>$project['publicPath'],
-            'privatePath'=>$project['privatePath'],
-        ]);
+        try {
+            return $this->targets->remoteRequest($projectId,'deploy-commit',[
+                'project'=>$projectId,
+                'uploadId'=>$uploadId,
+                'publicPath'=>$project['publicPath'],
+                'privatePath'=>$project['privatePath'],
+            ]);
+        } catch (RuntimeException $e) {
+            $message=$e->getMessage();
+            if (preg_match('/^(TARGET_CONNECT_FAILED_|TARGET_INVALID_RESPONSE_HTTP_50[234]|TARGET_HTTP_50[234])/', $message)) {
+                throw new RuntimeException('REMOTE_COMMIT_UNCERTAIN_'.$message, 0, $e);
+            }
+            throw $e;
+        }
     }
 
     public function rollback(string $projectId,array $project,string $release): array
